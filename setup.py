7928bdc3cc739ca6fbcb9508835fff8044a839bb
suraj-deshmukh <surajdeshmukh96@gmail.com>
2020-05-21 11:25:33 +0530

    version update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="Covid19India",
-    version="0.0.3",
+    version="0.0.4",
     description="A Python3 Library to get India's Covid-19 Patient Count.",
     long_description=README,
     long_description_content_type="text/markdown",
